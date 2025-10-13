28bebe095a47ae7811eeb66d1f82927c156e3e76
yuvraj249 <yuvraj@tech-x.com>
2025-10-13 17:25:31 +0530

    commit for changes

--- config/db.go
+++ config/db.go
@@ -1,10 +1,30 @@
 package config
 
 import (
-	"fmt"
 	"database/sql"
 	"os"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
+var DB *sql.DB
+
+func InitDB() {
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	dsn := os.Getenv("DSN")
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+}
