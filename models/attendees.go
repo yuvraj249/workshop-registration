ddd15b4552fb45f10142709ce3b3cc74e4f6f6fd
yuvraj249 <yuvraj@tech-x.com>
2025-10-14 12:30:35 +0530

    added functionalities for workshop table

--- models/attendees.go
+++ models/attendees.go
@@ -12,7 +12,7 @@ func InsertAttendee(name, email string) int64 {
 		panic(err)
 	}
 	id, _ := result.LastInsertId()
-	fmt.Printf("Id inserted : %d", id)
+	fmt.Println("Id inserted: ", id)
 	return id
 }
 
@@ -21,6 +21,7 @@ func GetAttendee() {
 	if err != nil {
 		panic(err)
 	}
+	defer rows.Close()
 
 	for rows.Next() {
 		var id int
@@ -29,4 +30,15 @@ func GetAttendee() {
 		fmt.Printf("Id: %d, Name: %s, Email: %s\n", id, name, email)
 
 	}
+	fmt.Println()
+}
+
+func DeleteAttendee(id int64) {
+	query := "DELETE FROM attendees WHERE id=?"
+	result, err := config.DB.Exec(query, id)
+	if err != nil {
+		panic(err)
+	}
+	rowsAffected, _ := result.RowsAffected()
+	fmt.Printf("Deleted attendee id : %d (Rows Affected : %d)\n", id, rowsAffected)
 }
