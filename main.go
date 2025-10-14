4bb3e30ef73b3f62d72438f7eb4a1db605386f13
yuvraj249 <yuvraj@tech-x.com>
2025-10-14 15:07:07 +0530

    final commit

--- main.go
+++ main.go
@@ -12,16 +12,13 @@ func main() {
 	att1 := models.InsertAttendee("Yuvraj", "yuvraj@example.com")
 	att2 := models.InsertAttendee("Akash", "akash@example.com")
 
-	// Add workshops
 	ws1 := models.InsertWorkshop("Go Concurrency", 2)
 	ws2 := models.InsertWorkshop("Building APIs with Go", 3)
 
-	// Register attendees
 	models.RegisterAttendee(int(att1), int(ws1))
 	models.RegisterAttendee(int(att2), int(ws1))
 	models.RegisterAttendee(int(att2), int(ws2))
 
-	// List attendees for a workshop
 	println("\nAttendees for Workshop 1:")
 	models.GetWorkshopAttendees(int(ws1))
 }
