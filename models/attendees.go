203603784303e823a78c7285c4284b7578886d1c
yuvraj249 <yuvraj@tech-x.com>
2025-10-14 11:44:39 +0530

    added functionalities for attendee table

--- models/attendees.go
+++ models/attendees.go
@@ -5,4 +5,28 @@ import (
 	"workshop-registration/config"
 )
 
-func InsertAttendees()
+func InsertAttendee(name, email string) int64 {
+	query := "INSERT INTO attendees(name,string) values(?,?)"
+	result, err := config.DB.Exec(query, name, email)
+	if err != nil {
+		panic(err)
+	}
+	id, _ := result.LastInsertId()
+	fmt.Printf("Id inserted : %d", id)
+	return id
+}
+
+func GetAttendee() {
+	rows, err := config.DB.Query("SELECT * FROM attendees")
+	if err != nil {
+		panic(err)
+	}
+
+	for rows.Next() {
+		var id int
+		var name, email string
+		rows.Scan(&id, &name, &email)
+		fmt.Printf("Id: %d, Name: %s, Email: %s\n", id, name, email)
+
+	}
+}
